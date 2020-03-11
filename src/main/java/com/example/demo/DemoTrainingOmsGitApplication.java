b685e755d1b17105832455788c5ee31eaa271a3a
Prerna <36573566+IMCHOUDHARY@users.noreply.github.com>
2020-03-11 14:56:04 +0530

    Commit 1

---
+++ src/main/java/com/example/demo/DemoTrainingOmsGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTrainingOmsGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTrainingOmsGitApplication.class, args);
+	}
+
+}
